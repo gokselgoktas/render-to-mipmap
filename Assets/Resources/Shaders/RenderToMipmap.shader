b6f778d57c414428ace75c90b592899ec6e227ac
Goksel Goktas <gokselgoktas@gmail.com>
2016-11-22 22:35:01 +0200

    Initial implementation

---
+++ Assets/Resources/Shaders/RenderToMipmap.shader
@@ -0,0 +1,115 @@
+﻿Shader "Hidden/Render to Mipmap"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+
+    CGINCLUDE
+    #include "UnityCG.cginc"
+
+    struct Input
+    {
+        float4 vertex : POSITION;
+        float2 uv : TEXCOORD0;
+    };
+
+    struct Varyings
+    {
+        float4 vertex : SV_POSITION;
+        float2 uv : TEXCOORD0;
+    };
+
+    sampler2D _MainTex;
+
+    float4 _MainTex_TexelSize;
+    float _SourceLevel;
+
+    Varyings vertex(Input input)
+    {
+        Varyings output;
+
+        float4 vertex = mul(UNITY_MATRIX_MVP, input.vertex);
+
+        output.vertex = vertex;
+        output.uv = input.uv.xy;
+
+#if UNITY_UV_STARTS_AT_TOP
+        if (_MainTex_TexelSize.y < 0)
+            output.uv.y = 1. - input.uv.y;
+#endif
+
+        return output;
+    }
+
+    float4 blitToMip0(in Varyings input) : SV_Target
+    {
+        return float4(1., 0., 0., 1.);
+    }
+
+    float4 blitToMip1(in Varyings input) : SV_Target
+    {
+        return float4(0., 1., 0., 1.);
+    }
+
+    float4 blitToMip2(in Varyings input) : SV_Target
+    {
+        return float4(0., 0., 1., 1.);
+    }
+
+    float4 blitToMip3(in Varyings input) : SV_Target
+    {
+        return float4(1., 1., 1., 1.);
+    }
+
+    float4 blit(in Varyings input) : SV_Target
+    {
+        return tex2Dlod(_MainTex, float4(input.uv, 0., _SourceLevel));
+    }
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertex
+            #pragma fragment blitToMip0
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertex
+            #pragma fragment blitToMip1
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertex
+            #pragma fragment blitToMip2
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertex
+            #pragma fragment blitToMip3
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vertex
+            #pragma fragment blit
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/RenderToMipmap.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RenderToMipmap : MonoBehaviour
+{
+    [Range(0, 3)]
+    public int sourceLevel = 0;
+
+    private Shader m_Shader;
+    public Shader shader
+    {
+        get
+        {
+            if (m_Shader == null)
+                m_Shader = Shader.Find("Hidden/Render to Mipmap");
+
+            return m_Shader;
+        }
+    }
+
+    private Material m_Material;
+    public Material material
+    {
+        get
+        {
+            if (m_Material == null)
+            {
+                if (shader == null || !shader.isSupported)
+                    return null;
+
+                m_Material = new Material(shader);
+            }
+
+            return m_Material;
+        }
+    }
+
+    private Camera m_Camera;
+    public Camera camera_
+    {
+        get
+        {
+            if (m_Camera == null)
+                m_Camera = GetComponent<Camera>();
+
+            return m_Camera;
+        }
+    }
+
+    private RenderTexture m_Mipmap;
+
+    private void RenderFullScreenQuad()
+    {
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        //Render the full screen quad manually.
+        GL.Begin(GL.QUADS);
+        GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(0.0f, 0.0f, 0.1f);
+        GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 0.0f, 0.1f);
+        GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, 0.1f);
+        GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(0.0f, 1.0f, 0.1f);
+        GL.End();
+
+        GL.PopMatrix();
+    }
+
+    void OnDisable()
+    {
+        if (m_Mipmap != null)
+        {
+            RenderTexture.ReleaseTemporary(m_Mipmap);
+            m_Mipmap = null;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (m_Mipmap == null || (m_Mipmap.width != source.width || m_Mipmap.height != source.height))
+        {
+            if (m_Mipmap)
+                RenderTexture.ReleaseTemporary(m_Mipmap);
+
+            m_Mipmap = RenderTexture.GetTemporary(source.width, source.height, 0, source.format, RenderTextureReadWrite.Default);
+            m_Mipmap.filterMode = FilterMode.Bilinear;
+
+            m_Mipmap.useMipMap = true;
+            m_Mipmap.autoGenerateMips = false;
+
+            m_Mipmap.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        material.SetFloat("_SourceLevel", (float) sourceLevel);
+
+        Graphics.SetRenderTarget(m_Mipmap, 0);
+        material.SetPass(0);
+        RenderFullScreenQuad();
+
+        Graphics.SetRenderTarget(m_Mipmap, 1);
+        material.SetPass(1);
+        RenderFullScreenQuad();
+
+        Graphics.SetRenderTarget(m_Mipmap, 2);
+        material.SetPass(2);
+        RenderFullScreenQuad();
+
+        Graphics.SetRenderTarget(m_Mipmap, 3);
+        material.SetPass(3);
+        RenderFullScreenQuad();
+
+        Graphics.Blit(m_Mipmap, destination, material, 4);
+    }
+}
